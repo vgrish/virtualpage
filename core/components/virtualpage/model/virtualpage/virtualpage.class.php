78bd66305a9dc95839590df5ade4879d6d889139
Володя Володин <vovanblya@mail.ru>
2015-03-13 12:55:34 +0400

    + plugin [build]

--- core/components/virtualpage/model/virtualpage/virtualpage.class.php
+++ core/components/virtualpage/model/virtualpage/virtualpage.class.php
@@ -7,6 +7,12 @@ class virtualpage {
 	/* @var modX $modx */
 	public $modx;
 
+	/** @var array $initialized */
+	public $initialized = array();
+
+	public $namespace = 'virtualpage';
+	public $config = array();
+	public $active = false;
 
 	/**
 	 * @param modX $modx
@@ -15,6 +21,7 @@ class virtualpage {
 	function __construct(modX &$modx, array $config = array()) {
 		$this->modx =& $modx;
 
+		$this->namespace = $this->getOption('namespace', $config, 'virtualpage');
 		$corePath = $this->modx->getOption('virtualpage_core_path', $config, $this->modx->getOption('core_path') . 'components/virtualpage/');
 		$assetsUrl = $this->modx->getOption('virtualpage_assets_url', $config, $this->modx->getOption('assets_url') . 'components/virtualpage/');
 		$connectorUrl = $assetsUrl . 'connector.php';
@@ -37,6 +44,29 @@ class virtualpage {
 
 		$this->modx->addPackage('virtualpage', $this->config['modelPath']);
 		$this->modx->lexicon->load('virtualpage:default');
+
+		$this->active = $this->modx->getOption('virtualpage_active', $config, false);
+
+	}
+
+	/**
+	 * @param $key
+	 * @param array $config
+	 * @param null $default
+	 * @return mixed|null
+	 */
+	public function getOption($key, $config = array(), $default = null) {
+		$option = $default;
+		if (!empty($key) && is_string($key)) {
+			if ($config != null && array_key_exists($key, $config)) {
+				$option = $config[$key];
+			} elseif (array_key_exists($key, $this->config)) {
+				$option = $this->config[$key];
+			} elseif (array_key_exists("{$this->namespace}.{$key}", $this->modx->config)) {
+				$option = $this->modx->getOption("{$this->namespace}.{$key}");
+			}
+		}
+		return $option;
 	}
 
 }
