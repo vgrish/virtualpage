f79a2bf017ad249f3aafd27d9cd4a11753f81f37
Володя Володин <vovanblya@mail.ru>
2015-03-13 09:38:53 +0400

    + manager lexicon [build]

--- core/components/virtualpage/lexicon/ru/default.inc.php
+++ core/components/virtualpage/lexicon/ru/default.inc.php
@@ -1,5 +1,9 @@
 <?php
-include_once 'setting.inc.php';
+
+$files = scandir(dirname(__FILE__));
+foreach ($files as $file) {
+	@include_once($file);
+}
 
 $_lang['virtualpage'] = 'virtualpage';
 $_lang['vp_menu_desc'] = 'Пример расширения для разработки.';
@@ -11,33 +15,3 @@ $_lang['vp_route_intro'] = 'Панель управления маршрутам
 
 $_lang['vp_event'] = 'События';
 $_lang['vp_event_intro'] = 'Панель управления событиями.';
-
-
-$_lang['virtualpage_intro_msg'] = 'Вы можете выделять сразу несколько предметов при помощи Shift или Ctrl.';
-$_lang['virtualpage_items'] = 'Предметы';
-$_lang['virtualpage_item_id'] = 'Id';
-$_lang['virtualpage_item_name'] = 'Название';
-$_lang['virtualpage_item_description'] = 'Описание';
-$_lang['virtualpage_item_active'] = 'Активно';
-
-$_lang['virtualpage_item_create'] = 'Создать предмет';
-$_lang['virtualpage_item_update'] = 'Изменить Предмет';
-$_lang['virtualpage_item_enable'] = 'Включить Предмет';
-$_lang['virtualpage_items_enable'] = 'Включить Предметы';
-$_lang['virtualpage_item_disable'] = 'Отключить Предмет';
-$_lang['virtualpage_items_disable'] = 'Отключить Предметы';
-$_lang['virtualpage_item_remove'] = 'Удалить Предмет';
-$_lang['virtualpage_items_remove'] = 'Удалить Предметы';
-$_lang['virtualpage_item_remove_confirm'] = 'Вы уверены, что хотите удалить этот Предмет?';
-$_lang['virtualpage_items_remove_confirm'] = 'Вы уверены, что хотите удалить эти Предметы?';
-$_lang['virtualpage_item_active'] = 'Включено';
-
-$_lang['virtualpage_item_err_name'] = 'Вы должны указать имя Предмета.';
-$_lang['virtualpage_item_err_ae'] = 'Предмет с таким именем уже существует.';
-$_lang['virtualpage_item_err_nf'] = 'Предмет не найден.';
-$_lang['virtualpage_item_err_ns'] = 'Предмет не указан.';
-$_lang['virtualpage_item_err_remove'] = 'Ошибка при удалении Предмета.';
-$_lang['virtualpage_item_err_save'] = 'Ошибка при сохранении Предмета.';
-
-$_lang['virtualpage_grid_search'] = 'Поиск';
-$_lang['virtualpage_grid_actions'] = 'Действия';
---
+++ core/components/virtualpage/lexicon/ru/manager.inc.php
@@ -0,0 +1,40 @@
+<?php
+
+$_lang['vp_menu_create'] = 'Создать';
+$_lang['vp_menu_add'] = 'Добавить';
+$_lang['vp_menu_update'] = 'Изменить';
+$_lang['vp_menu_remove'] = 'Удалить';
+$_lang['vp_menu_active'] = 'Активировать';
+
+// buttons
+$_lang['vp_btn_create'] = 'Создать';
+$_lang['vp_btn_save'] = 'Сохранить';
+$_lang['vp_btn_edit'] = 'Изменить';
+$_lang['vp_btn_view'] = 'Просмотр';
+$_lang['vp_btn_delete'] = 'Удалить';
+$_lang['vp_btn_cancel'] = 'Отмена';
+
+//
+$_lang['vp_actions'] = 'Действия';
+$_lang['vp_combo_select'] = 'Нажмите для выбора';
+
+$_lang['vp_id'] = 'Id';
+$_lang['vp_name'] = 'Имя';
+$_lang['vp_color'] = 'Цвет';
+
+$_lang['vp_active'] = 'Включен';
+$_lang['vp_description'] = 'Описание';
+$_lang['vp_num'] = 'Номер';
+$_lang['vp_status'] = 'Статус';
+$_lang['vp_count'] = 'Количество';
+
+$_lang['vp_all'] = 'Все';
+$_lang['vp_empty'] = 'Нет';
+
+$_lang['vp_createdon'] = 'Дата создания';
+$_lang['vp_updatedon'] = 'Дата изменения';
+$_lang['vp_timestamp'] = 'Метка времени';
+
+$_lang['vp_resource'] = 'Ресурс';
+$_lang['vp_comment'] = 'Комментарий';
+$_lang['vp_options'] = 'Опции';
