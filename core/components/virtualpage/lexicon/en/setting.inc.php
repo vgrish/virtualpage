6080a9dd0c6c777a82da16a8f155eca24f363ce0
Володя Володин <vovanblya@mail.ru>
2015-03-15 12:31:30 +0400

    + fix query  [build]

--- core/components/virtualpage/lexicon/en/setting.inc.php
+++ core/components/virtualpage/lexicon/en/setting.inc.php
@@ -4,9 +4,9 @@ $_lang['area_virtualpage_main'] = 'Main';
 
 $_lang['setting_virtualpage_active'] = 'Включить / Выключить VirtualPage';
 
-$_lang['setting_fastrouter_key'] = 'Ключ Fast Router';
-$_lang['setting_fastrouter_key_desc'] = 'Ключ в массиве $_REQUEST';
+$_lang['setting_virtualpage_fastrouter_key'] = 'Ключ Fast Router';
+$_lang['setting_virtualpage_fastrouter_key_desc'] = 'Ключ в массиве $_REQUEST';
 
-$_lang['setting_exclude_event_groupname'] = 'Исключить события';
-$_lang['setting_exclude_event_groupname_desc'] = 'Имена групп событий. Входящие в группу события будут исключены. ';
+$_lang['setting_virtualpage_exclude_event_groupname'] = 'Исключить события';
+$_lang['setting_virtualpage_exclude_event_groupname_desc'] = 'Имена групп событий. Входящие в группу события будут исключены. ';
 
