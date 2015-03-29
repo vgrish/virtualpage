f83a299fc7a6fc86a62bef7aaec63de9cc4a7556
Володя Володин <vovanblya@mail.ru>
2015-03-29 10:48:40 +0400

    + fix  [build]

--- core/components/virtualpage/lexicon/en/setting.inc.php
+++ core/components/virtualpage/lexicon/en/setting.inc.php
@@ -2,13 +2,13 @@
 
 $_lang['area_virtualpage_main'] = 'Main';
 
-$_lang['setting_virtualpage_active'] = 'Включить / Выключить VirtualPage';
+$_lang['setting_virtualpage_active'] = 'Enable / Disable VirtualPage';
 
-$_lang['setting_virtualpage_fastrouter_key'] = 'Ключ Fast Router';
-$_lang['setting_virtualpage_fastrouter_key_desc'] = 'Ключ в массиве $_REQUEST';
+$_lang['setting_virtualpage_fastrouter_key'] = 'Key Fast Router';
+$_lang['setting_virtualpage_fastrouter_key_desc'] = 'Key in $_REQUEST';
 
-$_lang['setting_virtualpage_exclude_event_groupname'] = 'Исключить события';
-$_lang['setting_virtualpage_exclude_event_groupname_desc'] = 'Имена групп событий. Входящие в группу события будут исключены. ';
+$_lang['setting_virtualpage_exclude_event_groupname'] = 'Exclude events';
+$_lang['setting_virtualpage_exclude_event_groupname_desc'] = 'The names of the groups of events. Included in the group events will be excluded.';
 
-$_lang['setting_prefix_placeholder'] = 'The prefix placeholders';
-$_lang['setting_prefix_placeholder_desc'] = 'The prefix placeholders in handleк. Default "vp."';
+$_lang['setting_virtualpage_prefix_placeholder'] = 'The prefix placeholders';
+$_lang['setting_virtualpage_prefix_placeholder_desc'] = 'The prefix placeholders in handler. Default "vp."';
--- core/components/virtualpage/lexicon/ru/setting.inc.php
+++ core/components/virtualpage/lexicon/ru/setting.inc.php
@@ -4,11 +4,11 @@ $_lang['area_virtualpage_main'] = 'Основные';
 
 $_lang['setting_virtualpage_active'] = 'Включить / Выключить VirtualPage';
 
-$_lang['setting_fastrouter_key'] = 'Ключ Fast Router';
-$_lang['setting_fastrouter_key_desc'] = 'Ключ в массиве $_REQUEST';
+$_lang['setting_virtualpage_fastrouter_key'] = 'Ключ Fast Router';
+$_lang['setting_virtualpage_fastrouter_key_desc'] = 'Ключ в массиве $_REQUEST';
 
-$_lang['setting_exclude_event_groupname'] = 'Исключить события';
-$_lang['setting_exclude_event_groupname_desc'] = 'Имена групп событий. Входящие в группу события будут исключены. ';
+$_lang['setting_virtualpage_exclude_event_groupname'] = 'Исключить события';
+$_lang['setting_virtualpage_exclude_event_groupname_desc'] = 'Имена групп событий. Входящие в группу события будут исключены. ';
 
-$_lang['setting_prefix_placeholder'] = 'Префикс плейсходеров';
-$_lang['setting_prefix_placeholder_desc'] = 'Префикс плейсходеров в обработчике. По умолчанию "vp."';
+$_lang['setting_virtualpage_prefix_placeholder'] = 'Префикс плейсходеров';
+$_lang['setting_virtualpage_prefix_placeholder_desc'] = 'Префикс плейсходеров в обработчике. По умолчанию "vp."';
